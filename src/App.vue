8ab7ed98498ec05de40e0fa387b08288d8d26b32
Hotohara <hot87hara@gmail.com>
2022-08-26 08:13:02 +0900

    first commit at main

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+// import HelloWorld from './components/HelloWorld.vue'
+import Udemy from './components/Udemy.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld_xx msg="Vite + Vue" /> -->
+  <Udemy />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Udemy.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>{{ message }}</p>
+  <input type="text" v-model="message" />aa
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "Helloo" }
+  },
+  watch: {
+    message: function(newValue, oldValue) {
+      console.log('New:', newValue, 'Old:', oldValue)
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
