f72394643a27d021c8e5656387f17e0cb144d088
Hotohara <hot87hara@gmail.com>
2022-08-27 08:51:20 +0900

    set7 computed and watched

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-
 // import HelloWorld from './components/HelloWorld.vue'
 import Udemy from './components/Udemy.vue'
+
 </script>
 
 <template>
   <!-- <HelloWorld_xx msg="Vite + Vue" /> -->
-  <Udemy />
+  <Udemy  />
 </template>
 
 <style scoped>
--- src/components/Udemy.vue
+++ src/components/Udemy.vue
@@ -1,26 +1,36 @@
 <template>
-  <p><input type="text" v-model="km" />km</p>
-  <p><input type="text" v-model="m" />m</p>
+  <p>first:<input type="text" v-model="first" /></p>
+  <p>last:<input type="text" v-model="last" /></p>
+  <p>Full Name: {{ fullName }}</p>
 </template>
+
+<script setup>
+</script>
 
 <script>
 export default {
   data() {
     return {
-      km: 0,
-      m: 0
+      first: "",
+      last: "",
+      // fullName: ""
     }
   },
-  watch: {
-    km: function(value) {
-      console.log('km:', value)
-      this.m = value * 1000
-    },
-    m: function(value) {
-      console.log('m:', value)
-      this.km = value / 1000
-    },
+  computed: {
+    fullName: function() {
+      return this.first + ',' + this.last
+    }
   }
+  // watch: {
+  //   first: function(value) {
+  //     this.fullName = value + ' ' + this.last
+  //     console.log('first:', value)
+  //   },
+  //   last: function(value) {
+  //     this.fullName = this.first + ' ' + value
+  //     console.log('last:', value)
+  //   },
+  // }
 }
 </script>
 
