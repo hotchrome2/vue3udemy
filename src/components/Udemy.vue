8b2fefa106c3717431149dfab2979c0bac5ab317
Hotohara <hot87hara@gmail.com>
2022-08-28 19:06:26 +0900

    empty data

--- src/components/Udemy.vue
+++ src/components/Udemy.vue
@@ -1,10 +1,4 @@
 <template>
-  <ul>
-    <li v-for="color in colors">
-      {{ color.name}}
-    </li>
-  </ul>
-  <button v-on:click="onClick">Click here</button>
 </template>
 
 <script setup>
@@ -14,34 +8,16 @@
 export default {
   data() {
     return {
-      colors : [
-        { name: 'Red'},
-        { name: 'Green'},
-        { name: 'Blue'},
-      ],
-      index: 0,
-      count: 0,
+      items: null,
+      keyword: '',
+      message: ''
     }
   },
   watch: {
-    colors: {
-      handler: function(newValue, oldValue) {
-        // 本当に新旧の値が違う時のみここに来る
-        this.count++;
-        console.log('Updated. count:', this.count)
-      },
-      deep: true
-    }
+  },
+  mounted: {
   },
   methods: {
-    onClick: function(event) {
-      this.index++;
-      if (this.index >= 3) {
-        this.index = 0;
-      }
-      console.log('index:', this.index)
-      this.colors[this.index].name = 'White'
-    }
   }
 }
 </script>
