5743f349a2302113c4fd345c909bbcb808bf8b42
Hotohara <hot87hara@gmail.com>
2022-08-28 19:30:32 +0900

    axios get

--- src/components/Udemy.vue
+++ src/components/Udemy.vue
@@ -5,6 +5,7 @@
 </script>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -15,9 +16,37 @@ export default {
   },
   watch: {
   },
-  mounted: {
+  mounted: function() {
+    this.keyword = 'JavaScript'
+    this.getAnswer()
   },
   methods: {
+    getAnswer: function() {
+      if (this.keyword === '') {
+        console.log('karamoji')
+        this.items = null
+        return
+      }
+      this.message = 'Loading..'
+      // thisをローカル変数に入れておく
+      const vm = this
+
+      const params = { page: 1, per_page: 10, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', { params })
+      .then(function(response){
+        console.log('api response:')
+        console.log(response)
+      })
+      .catch(function(error){
+        console.log(error)
+        vm.message = 'Error:' + error
+      })
+      .finally(function() {
+        console.log("finally.")
+        vm.message = ""
+      })
+
+    }
   }
 }
 </script>
