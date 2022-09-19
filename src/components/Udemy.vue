b8ccdf2d721ab89bf51a76f9dc4a63b47832e9fb
Hotohara <hot87hara@gmail.com>
2022-09-19 19:23:54 +0900

    sec10-81 v-else-if

--- src/components/Udemy.vue
+++ src/components/Udemy.vue
@@ -1,7 +1,15 @@
 <template>
-  <p>sec10 80</p>
-  <p v-if="toggle">Yes</p>
-  <p v-else>No</p>
+  <p>sec10 81</p>
+  <p v-if="color === 'red'">
+    Stop
+  </p>
+  <p v-else-if="color === 'yellow'">
+    Caution
+  </p>
+  <p v-else-if="color === 'blue'">
+    Go
+  </p>
+  <p v-else>Not red/yellow/blue</p>
 </template>
 
 <script setup>
@@ -11,15 +19,9 @@
 export default {
   data() {
     return {
-      toggle: false,
+      color: 'yellow',
     }
   },
-  watch: {
-  },
-  mounted: function() {
-  },
-  methods: {
-  }
 }
 </script>
 
